41cef88d061a314156c1923a92903fc93e1dfc0b
y4nk1 <66532537+y4nk1@users.noreply.github.com>
2024-04-20 19:44:38 -0400

    Create y4nk1.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/y4nk1.py
@@ -0,0 +1,18 @@
+# URL:https://www.python.org/
+
+"""
+esto es un comentario
+de varias
+lineas
+"""
+variable = 0
+#las constantes en python se crean escribiendo el nombre en mayusculas y se deben respetar todas las variables escritas en mayusculas
+CONSTANTE = 5
+
+enteros = 7
+float = 1.5
+string = "cadena de texto"
+booleanos_verdadero = true
+booleano_falso = false
+print("hola python")
+
